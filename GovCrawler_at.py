95f4930bb1c9ae2e9c01a021c78ef155d158c1c9
Erwin Ernst Steinhammer <eestoneee@gmail.com>
2020-01-18 02:48:05 +0100

    minor optimisations

--- GovCrawler_at.py
+++ GovCrawler_at.py
@@ -2,9 +2,11 @@ import os
 import json
 from mastodon import Mastodon
 
-protocol_dir = "data/protokolle/Kurz-2/Beschlussprotokoll des 2. Ministerrates vom 15. Jänner 2020/text.json"
+protocol_dir = "data/protokolle/Kurz-2/"
+last_protocol = "Beschlussprotokoll des 2. Ministerrates vom 15. Jänner 2020"
+protocol_text_json = "/text.json"
 
-text_file = os.path.join(protocol_dir)
+text_file = os.path.join(protocol_dir + last_protocol + protocol_text_json)
 
 with open(text_file, 'r') as f:
     data=f.read()
@@ -16,11 +18,8 @@ mastodon = Mastodon(
     api_base_url = 'https://botsin.space'
 )
 
-last_toot = 0
-
+#create a thread with the latest protocol
+toot = toot = mastodon.status_post(last_protocol + " #test", visibility="unlisted", spoiler_text="test, Hackathon 0x11")
 for i in protocol:
     print("Top " + protocol[protocol.index(i)]['top'] + ": " + protocol[protocol.index(i)]['title'])
-    if protocol.index(i) == 0:
-        last_toot = mastodon.status_post("Top " + protocol[protocol.index(i)]['top'] + ": " + protocol[protocol.index(i)]['title'] + " #test", visibility="unlisted", spoiler_text="test, Hackathon 0x11")
-    else:
-        last_toot = mastodon.status_reply(last_toot,"Top " + protocol[protocol.index(i)]['top'] + ": " + protocol[protocol.index(i)]['title'] + " #test")
+    toot = mastodon.status_reply(toot,"Top " + protocol[protocol.index(i)]['top'] + ": " + protocol[protocol.index(i)]['title'] + " #test")
