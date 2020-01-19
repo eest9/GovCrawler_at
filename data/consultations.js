b1727fe8740c50a9e5210b935792ce1d4df9a9be
Erwin Ernst Steinhammer <eestoneee@gmail.com>
2020-01-19 19:24:35 +0100

    minor fixes

--- data/consultations.js
+++ data/consultations.js
@@ -1,12 +1,13 @@
 var consultations_json = new XMLHttpRequest();
 consultations_json.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
-    //consultations = JSON.parse(this.responseText);
+    consultations = JSON.parse(this.responseText);
     var top_element = document.createElement("P");// Create a <p> element
     top_element.id = "consulationt_bill-1";// Insert text
     top_element.className += "consulation_bill";
-    top_element.innerHTML = "test";
-    //top_element.innerHTML = consultations.OgdDocumentResults.OgdDocumentReference[1].Data.Metadaten.Bundesgesetzblaetter.Titel;
+    //top_element.innerHTML = "test";
+    console.log(consultations);
+    top_element.innerHTML = consultations.OgdSearchResult.OgdDocumentResults.OgdDocumentReference[1].Data.Metadaten.Bundesgesetzblaetter.Titel;
     document.getElementById("consultations").appendChild(top_element);
   }
 };
